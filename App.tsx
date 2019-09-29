9088424470d90073d709f94cf6cbbc1c9c20032f
David McNamee <davidmcnamee314@gmail.com>
2019-09-29 01:25:53 -0400

    testing (incomplete)

--- App.tsx
+++ App.tsx
@@ -3,50 +3,46 @@ import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as Permissions from 'expo-permissions';
 
-export default class App extends React.Component{
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-  }
-  
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
+export default function App() {
 
-  render() {
+  const [ hasCameraPermission, setCameraPermission ] = useState(null);
+  const [ type, setType ] = useState(Camera.Constants.Type.back);
 
-    return (
-      <View style={{ flex: 1 }}>
-        { this.state.hasCameraPermission && (
-            <Camera style={{ flex: 1 }} type={this.state.type}>
-              <View style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-                }}>
-                <TouchableOpacity style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center'
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  });
-                }}>
-                  <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-                </TouchableOpacity>
-              </View>
-            </Camera>
-        )}
-        { !this.state.hasCameraPermission && (
-          <Text>No camera permissions :(</Text>
-        )}
-      </View>
-    );
-  }
+  useEffect(() => {
+    Permissions.askAsync(Permissions.CAMERA).then(({ status }) => {
+      setCameraPermission(status === 'granted');
+    });
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      { hasCameraPermission && (
+          <Camera style={{ flex: 1 }} type={type}>
+            <View style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              }}>
+              <TouchableOpacity style={{
+                flex: 0.1,
+                alignSelf: 'flex-end',
+                alignItems: 'center'
+              }}
+              onPress={() => {
+                setType(
+                    type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                );
+              }}>
+                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+      )}
+      { !hasCameraPermission && (
+        <Text>No camera permissions :(</Text>
+      )}
+    </View>
+  );
 }
