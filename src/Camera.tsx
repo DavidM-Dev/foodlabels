89a4d7a377745c8f3a639d2bddbffd93d7eee35e
David McNamee <davidmcnamee314@gmail.com>
2019-09-29 15:13:50 -0400

    remove precommit linting

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -5,13 +5,7 @@ import { View } from "react-native";
 export default function Camera() {
   return (
     <ExpoCamera style={{ flex: 1 }} type={ExpoCamera.Constants.Type.back}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "transparent",
-          flexDirection: "row"
-        }}
-      />
+      <View style={{ flex: 1, backgroundColor: "transparent", flexDirection: "row" }} />
     </ExpoCamera>
   );
 }
